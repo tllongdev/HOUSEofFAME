c919c5b4d5c24ab6f5fedab9dd6832ab1c46c946
Timothy Lee Long <tllongdev@gmail.com>
2020-05-27 15:06:22 -0500

    temporary flier setup

---
+++ public/precache-manifest.f86df75355fff4af28824a1df74f6257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b44a58c6c3aaa3f3fc357c80f25b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4aedbf68b394a4aa8d3",
+    "url": "/static/css/main.11801db5.chunk.css"
+  },
+  {
+    "revision": "3aef7055338101cc3b50",
+    "url": "/static/js/2.bc8e5735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc8e5735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4aedbf68b394a4aa8d3",
+    "url": "/static/js/main.4cf6e7ff.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "ad909e982602bbeaa43c0303cf218751",
+    "url": "/static/media/HOFFASHIONflier.ad909e98.jpg"
+  }
+]);
